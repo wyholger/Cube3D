bfbee0028ddd45f001132e757d0fe1ae81671362
Artem Makarov <52400119+makarbass@users.noreply.github.com>
2022-04-21 21:58:20 +0300

    Add files via upload

--- src/mlx/mlx.c
+++ src/mlx/mlx.c
@@ -2,8 +2,18 @@
 
 int	key_hook(int key_hook, t_data *data)
 {
+	mlx_clear_window(data->mlx.mlx, data->mlx.mlx_win);
+	if (key_hook == W)
+		data->player.y -= 0.1;
+	if (key_hook == S)
+		data->player.y += 0.1;
+	if (key_hook == A)
+		data->player.x -= 0.1;
+	if (key_hook == D)
+		data->player.x += 0.1;
 	if (key_hook == ESC)
 		exit_standard(data);
+	minimap(data);
 	return (0);
 }
 
@@ -15,5 +25,7 @@ void	mlx_initialise(t_data *data)
 	data->mlx.mlx_win = mlx_new_window(data->mlx.mlx, 1000, 600, "Coub3d");
 	if (data->mlx.mlx_win == NULL)
 		exit_after_validate(data, 9);
-	mlx_key_hook(data->mlx.mlx_win, key_hook, data);
+	mlx_hook(data->mlx.mlx_win, 2, 1<<3, key_hook, data);
+	//mlx_loop_hook(data->mlx.mlx, minimap, data); 
+	//mlx_key_hook(data->mlx.mlx_win, key_hook, data);
 }
